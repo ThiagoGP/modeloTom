e103a3f4fa0b521439e2af601fc80c2779fead73
ThiagoGP <62674239+ThiagoGP@users.noreply.github.com>
2025-06-11 07:55:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ if len(sys.argv) < 2:
 email = sys.argv[1]
 
 
-p1 = subprocess.Popen(["python", "requisicao.py", email])
+p1 = subprocess.Popen(["python3.8", "requisicao.py", email])
 print("requisição.py iniciado.")
 
 
 time.sleep(3)
 
 
-p2 = subprocess.Popen(["python", "modelo.py", email])
+p2 = subprocess.Popen(["python3.8", "modelo.py", email])
 print("modelo.py iniciado.")
 
 
